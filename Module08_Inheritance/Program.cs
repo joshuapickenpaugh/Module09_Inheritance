6a8fa59a4b5038a81a9619b278767f854be53e5c
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-10-07 17:33:47 -0500

    !

--- Module08_Inheritance/Program.cs
+++ Module08_Inheritance/Program.cs
@@ -10,6 +10,30 @@ namespace Module08_Inheritance
     {
         static void Main(string[] args)
         {
+            //Object instantiation:
+            Duck duck1 = new Duck();
+            duck1.Hello();
+
+            //Inherited from Animal class:
+            duck1.Sound();
+            
+            Console.ReadKey();
+        }
+    }
+
+    class Animal
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Noise!!!");
+        }
+    }
+
+    class Duck : Animal
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello from Class Duck, Method Hello.");
         }
     }
 }
